81f85add5d4a1b5a0337e15b9f00b655d7f59288
Caroline Smyth <caroline.smyth@gmail.com>
2025-06-12 10:06:25 -0400

    standard brownian motion

---
+++ script.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+rng = np.random.default_rng(42)
+
+paths = 50
+points = 1000
+
+mu, sigma = 0.0, 1.0
+
+Z = rng.normal(mu, sigma, (paths, points))
+
+interval = [0.0, 1.0]
+dt = (interval[1] - interval[0]) / (points - 1)
+
+t_axis = np.linspace(interval[0], interval[1], points)
+
+W = np.zeros((paths, points))
+for idx in range(points - 1):
+  real_idx = idx + 1
+  W[:, real_idx] = W[:, real_idx - 1] + np.sqrt(dt) * Z[:, idx]
+
+fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+for path in range(paths):
+  ax.plot(t_axis, W[path, :])
+ax.set_title("Standard Brownian Motion sample paths")
+ax.set_xlabel("Time")
+ax.set_ylabel("Asset Value")
+plt.show()
